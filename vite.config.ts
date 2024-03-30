57d2214428af95964915f491b35b549f3a10bd29
Choi Hong Gyu <zxolqs@gmail.com>
2024-03-30 16:03:24 +0900

    edit config file for publish package

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 /// <reference types="vitest" />
 import path from "path";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -16,4 +18,27 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: "./src/setup/test.ts"
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '@emotion/react', '@emotion/styled'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@emotion/react': '@emotion/react',
+          '@emotion/styled': '@emotion/styled',
+        },
+      },
+    },
+    commonjsOptions: {
+      esmExternals: ['react'],
+    },
+  }
 })
+
